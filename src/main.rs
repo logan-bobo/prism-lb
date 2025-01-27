0c158d2f5ab4ba46dd14c476992bac4191f18b81
Logan Cox <mail@logan-cox.com>
2025-01-27 09:06:26 +0000

    feat: add basic TCP listener on a single thread

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
+fn handle_connection(stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {http_request:#?}");
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting LB");
+
+    let listener = TcpListener::bind("127.0.0.1:5001").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
 }
